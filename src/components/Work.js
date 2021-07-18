3283f6083c5d9fa44d13aa68c31c51412a87c3ae
CeeDyyy <thanawat.ud@mail.kmutt.ac.th>
2021-07-19 03:38:22 +0700

    [Update] Update description to Eng

--- src/components/Work.js
+++ src/components/Work.js
@@ -13,7 +13,7 @@ function Work() {
                     <Modal
                         img="https://img.youtube.com/vi/pxMvtyBELxQ/sddefault.jpg"
                         title="Smart Home - Youth Leadership for Smart Energy in Thailand"
-                        description="วิดีโอนี้เป็นวิดีโอสัมภาษณ์นักศึกษาใน King Mongkut's University of Technology Thonburi ผู้สร้างเทคโนโลยีที่ Sustainable energy โดยนำมาแมพกันเป็น Smart Home โดยผมทำหน้าที่ตัดต่อ และทำ motion graphics ทุกส่วนในวิดีโอนี้ และมีการร่วมถ่ายทำด้วยบางส่วน"
+                        description="This video is an interview of the many students in King Mongkut's University of Technology Thonburi, Creators of a sustainable energy technology which they map them together into a Smart home system. As for my positions in this project, I edited and made all of the motion graphics seen in this video. I've also shot some parts of the interview."
                         url="https://www.youtube-nocookie.com/embed/pxMvtyBELxQ"
                     />
                 </div>
@@ -21,7 +21,7 @@ function Work() {
                     <Modal
                         img="https://img.youtube.com/vi/xBbYbl4wmy4/sddefault.jpg"
                         title="Trendy Eco Products and Design from used Meterials in Thailand - Ecotopia"
-                        description="วิดีโอนี้เป็นวิดีโอสัมภาษณ์ Dr. Singh เจ้าของ Eco Product ที่มีคุณภาพและ Trendy ที่ Ecotopia และ คุณ Pornthep, product designer เพื่อสัมภาษณ์ความคิดเห็นเกี่ยวกับการขาย eco product โดยผมทำหน้าที่ตัดต่อทุกส่วนในวิดีโอนี้ และถ่ายทำในบางส่วน"
+                        description="This video is an interview of Dr. Singh, The owner of the high quality and trendy Eco Products at Ecotopia and Mr.Pornthep, a product designer to interviewand discuss about the insight regarding about the eco products market. and as for my jobs, I edited all of the interview including making the Intro, and i have shot some of the parts."
                         url="https://www.youtube-nocookie.com/embed/xBbYbl4wmy4"
                     />
                 </div>
@@ -29,7 +29,7 @@ function Work() {
                     <Modal
                         img="https://img.youtube.com/vi/gUHn6o1og_0/sddefault.jpg"
                         title="Sustain Organic Dairy farm in Thailand - Dairy Home"
-                        description="วิดีโอนี้เป็นวิดีโอสัมภาษณ์ คุณ Pruitt เจ้าของฟาร์มนมออร์แกนิคเจ้าแรกของไทย Dairy Home ว่า how he believes everyone should turn to organic farming of dairy products. โดยผมทำหน้าที่ตัดต่อทุกส่วนในวิดีโอนี้ รวมถึงทำ Intro และถ่ายทำในบางส่วน"
+                        description="This video is an interview of Mr.Pruitt, The founder of Thailand's first organic milk farm, Dairy Home. to discuss about how he believes everyone should turn to organic farming of dairy products. I myself handled the editing process of the whole interview including making the Intro, I've also shot some parts of the video."
                         url="https://www.youtube-nocookie.com/embed/gUHn6o1og_0"
                     />
                 </div>
@@ -37,7 +37,7 @@ function Work() {
                     <Modal
                         img="https://img.youtube.com/vi/fcG_4GN9seY/sddefault.jpg"
                         title="Sustainability Business Features 2020 | Start-ups Towards Sustainability"
-                        description="วิดีโอนี้เป็นวิดีโอสัมภาษณ์ Start-ups หน้าใหม่ ที่ Towards Sustainability โดยผมทำหน้าที่ตัดต่อทุกส่วนในวิดีโอนี้"
+                        description="This video is an interview of the up and coming Start-ups at Towards Sustainability, I handled all of the editing process of the video."
                         url="https://www.youtube-nocookie.com/embed/fcG_4GN9seY"
                     />
                 </div>
@@ -45,7 +45,7 @@ function Work() {
                     <Modal
                         img="https://drive.google.com/thumbnail?id=1iBn93JLDpW94WoreaEm9g8SPH_nTbcDx"
                         title="Pre-IT#26 Starter Pack - Educational Video Intro"
-                        description="วิดีโอนี้เป็น Intro ของวิดีโอสอนรุ่นน้องปี 1 ในโครงการ bootcamp โดยผมเป็นคน animate Intro ตัวนี้"
+                        description="This is an Intro of the first year freshmen videos in the bootcamp, I myself animated the said Intro."
                         url="https://drive.google.com/file/d/1iBn93JLDpW94WoreaEm9g8SPH_nTbcDx/preview"
                     />
                 </div>
@@ -53,7 +53,7 @@ function Work() {
                     <Modal
                         img="https://drive.google.com/thumbnail?id=1NVTn5oyhahXhO4Yxwxm4VQ0qIMGiitg4"
                         title="Pre-IT#26 Starter Pack - Waiting... Looping Video"
-                        description="วิดีโอนี้เป็นวิดีโอสำหรับการเปิด Loop ในระหว่างรอรุ่นน้องปี 1 มา โดยผมเป็นคน animate animation ในวิดีโอนี้"
+                        description="This video is made specifically as a loop for waiting for the freshmen first year, The loop animation was done by me."
                         url="https://drive.google.com/file/d/1NVTn5oyhahXhO4Yxwxm4VQ0qIMGiitg4/preview"
                     />
                 </div>
@@ -61,23 +61,23 @@ function Work() {
                     <Modal
                         img="https://drive.google.com/thumbnail?id=1esxfLyYsCwSddI8i7x_gD3LWxa8mxQ0X"
                         title="WebTech Ep.1: Website & HTML | เริ่มต้นเขียนเว็บด้วยการเขียน HTML พื้นฐาน"
-                        description="วิดีโอนี้เป็นวิดีโอสอนรุ่นน้องปี 1 เกี่ยวกับการเริ่มต้นเขียนเว็บด้วยการเขียน HTML พื้นฐาน โดยผมทำหน้าที่ตัดต่อ และพากย์เสียงสอนในบางส่วน"
+                        description="This video is an instructional video intend to teach and introduce First-year students to writing elementary HTML websites, I've edited and dubbed in some parts of this video."
                         url="https://drive.google.com/file/d/1esxfLyYsCwSddI8i7x_gD3LWxa8mxQ0X/preview"
                     />
                 </div>
                 <div className="col">
                     <Modal
-                        img="https://drive.google.com/thumbnail?id=18bl0FLOR3bfTMkfAEIUebr9XD_2zB_I6"
+                        img="https://drive.google.com/thumbnail?id=1lXy99hUmLEbsZkhSjgcuMQrPDTUxtXxx"
                         title="WebTech Ep.4: List & Table | รู้จักกับการสร้างรายการและการสร้างตารางด้วย HTML"
-                        description="วิดีโอนี้เป็นวิดีโอสอนรุ่นน้องปี 1 เกี่ยวกับการสร้างรายการและการสร้างตารางด้วย HTML โดยผมทำหน้าที่ตัดต่อ และพากย์เสียงสอน"
-                        url="https://drive.google.com/file/d/18bl0FLOR3bfTMkfAEIUebr9XD_2zB_I6/preview"
+                        description="This video is an instructional video aimed to teach the First-year students about Creating List & Table with HTML, as for I, I've edited and dubbed in some parts of this video."
+                        url="https://drive.google.com/file/d/1lXy99hUmLEbsZkhSjgcuMQrPDTUxtXxx/preview"
                     />
                 </div>
                 <div className="col">
                     <Modal
                         img="https://drive.google.com/thumbnail?id=1ihcdFb7O8CNWBOote3WtcuItf_EmLgZt"
-                        title="It's Me | Trailer (2020) | General Education Subject in the Curriculum (GEN - 241)"
-                        description="วิดีโอนี้เป็นวิดีโอตัวอย่างละครเวทีเรื่อง It's Me ซึ่งเป็นส่วนหนึ่งของวิชา General Education Subject in the Curriculum (GEN - 241) โดยผมทำหน้าที่เขียนบท ทำ storyboard รวมถึงตัดต่อและทำสีในวิดีโอนี้"
+                        title="It's Me | Trailer (2020) | Beauty of Life (GEN - 241)"
+                        description="This video is a trailer for The stage play &ldquo;It's Me&rdquo; which is a part of Beauty of Life (GEN - 241). I handled the writing process and storyboard. I also edited and color graded this video."
                         url="https://drive.google.com/file/d/1ihcdFb7O8CNWBOote3WtcuItf_EmLgZt/preview"
                     />
                 </div>
@@ -85,7 +85,7 @@ function Work() {
                     <Modal
                         img="https://drive.google.com/thumbnail?id=1OvXdY9bre138xg_VjhOg0z-6Z_OJtDDm"
                         title="เทศกาลแห่งความรักของจีน - GEN 242 - Chinese Philosophy and Ways of Life"
-                        description="วิดีโอนี้เป็นวิดีโอเกี่ยวกับเทศกาลแห่งความรักของจีน ซึ่งเป็นส่วนหนึ่งของวิชา Chinese Philosophy and Ways of Life (GEN - 242) โดยผมทำหน้าที่สร้างและดีไซน์เลเวลเกมด้วย RPG Maker เพื่อเป็นสื่อสำหรับการเล่าเรื่อง See more : https://drive.google.com/drive/folders/1g6EqPzPHpSD-c-6NB4fhdFeOonLVz4ne?usp=sharing"
+                        description="This video is mainly about the chinese love festival, which is a part of Chinese Philosophy and Ways of Life (GEN - 242). as for me, I handled the making and level designing process using RPG Maker as a storytelling media. See more : https://drive.google.com/drive/folders/1g6EqPzPHpSD-c-6NB4fhdFeOonLVz4ne?usp=sharing"
                         url="https://drive.google.com/file/d/1OvXdY9bre138xg_VjhOg0z-6Z_OJtDDm/preview"
                     />
                 </div>
