3e3fa3de7f66c834dea7a2fae7ce4c8bb4d12256
CeeDyyy <thanawat.ud@mail.kmutt.ac.th>
2021-07-18 14:21:26 +0700

    [Add] Career Objective

---
+++ src/components/CareerObj.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./CareerObj.css"
+
+function CareerObj() {
+    return (
+        <div className="careerobj-bg">
+            <div className="careerobj-overlay">
+                <div className="container">
+                    <div className="careerobj-con">
+                        <div className="careerobj-text">
+                            <h1>CAREER OBJECTIVE</h1>
+                            <br />
+                            <p>
+                                I am desirous of the position of a Video Editor with an experienced team to expand my learnings, knowledge, and skills to ensure the quality of the work moving forward to utilize those knowledge and skills to be a part of driving Thai gaming industry to the global stage.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CareerObj
