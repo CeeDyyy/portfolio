fda68e8d9cb6f57cdb68aefa7afcdcb78b2d7c60
CeeDyyy <thanawat.ud@mail.kmutt.ac.th>
2021-07-18 14:21:12 +0700

    [Add] About Me

---
+++ src/components/AboutMe.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./AboutMe.css"
+
+function AboutMe() {
+    return (
+        <div className="container">
+            <section className="content-con">
+                <div className="content-l">
+                    <img src="https://scontent.fbkk2-6.fna.fbcdn.net/v/t1.6435-9/52788237_1287681174722359_5529166160420929536_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=174925&_nc_ohc=3ythAzmMRP4AX8JRVej&tn=s9iZSU-3TXm5mqAn&_nc_ht=scontent.fbkk2-6.fna&oh=905d324a399d03cae45abe05b31056c8&oe=60F70F10" alt="" />
+                </div>
+                <div className="content-r">
+                    <h2>ABOUT ME</h2>
+                    <p>
+                        <br />I am a fourth-year student with almost 2 years experience, working closely with project coordinator and producer in editing videos, mostly interviews videos. <br />I am also a camera operator and create motion graphics in some projects. I usually use Adobe Premiere Pro for editing videos, Adobe After Effects and Adobe Illustrator for creating motion graphics.
+                    </p>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default AboutMe
