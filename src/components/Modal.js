8f7a83b76dac9a239c37c5dc5e5ad15d90ae7b47
Thanawat <cdnoob@gmail.com>
2021-12-25 13:42:17 +0700

    reactstrap's modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,19 +1,15 @@
 import React, { useState } from "react";
 import "./Modal.css"
 import Card from './Card'
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 
-function Modal(props) {
+function WorkModal(props) {
     const [modal, setModal] = useState(false);
+    const [modall, setModall] = useState(false);
 
     const toggleModal = () => {
         setModal(!modal);
     };
-
-    if (modal) {
-        document.body.classList.add('active-modal')
-    } else {
-        document.body.classList.remove('active-modal')
-    }
 
     const monthNames = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
@@ -31,9 +27,33 @@ function Modal(props) {
                 />
             </button>
 
-            {modal && (
+            <Modal
+                centered
+                fullscreen="md"
+                scrollable
+                size="lg"
+                toggle={toggleModal}
+                isOpen={modal}
+            >
+                <ModalHeader toggle={toggleModal}>
+                    {props.title}
+                </ModalHeader>
+                <ModalBody>
+                    <div className="d-flex justify-content-center">
+                        <iframe width="560" height="315" src={props.url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                    <br />
+                    <div className="modal-body">
+                        <h6 className="text-black-50 pb-2">{props.date.day} {monthNames[Number(props.date.month) - 1]} {props.date.year}</h6>
+                        <p>{props.description}</p>
+                    </div>
+                </ModalBody>
+            </Modal>
+
+            {/* {modal && (
                 <div className="modal-modal">
-                    <div onClick={toggleModal} className="modal-overlay"></div>
+                    <div onClick={toggleModal} className="modal-overlay"></div> */}
+
                     {/* <div className="modal-content center">
                         <iframe width="560" height="315" src={props.url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         <br />
@@ -45,7 +65,8 @@ function Modal(props) {
                             x
                         </button>
                     </div> */}
-                    <div className="modal-dialog modal-lg modal-fullscreen-md-down modal-dialog-centered">
+
+                    {/* <div className="modal-dialog modal-lg modal-fullscreen-md-down modal-dialog-centered">
                         <div className="modal-content">
                             <div className="modal-header">
                                 <h5 className="modal-title" id="exampleModalLabel">{props.title}</h5>
@@ -64,29 +85,9 @@ function Modal(props) {
                         </div>
                     </div>
                 </div>
-            )}
-
-            {/* <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-lg modal-fullscreen-md-down modal-dialog-centered">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">{props.title}</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            <div className="d-flex justify-content-center">
-                                <iframe width="560" height="315" src={props.url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                            </div>
-                            <br />
-                            <div className="modal-body">
-                                <p>{props.description}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
+            )} */}
         </>
     );
 }
 
-export default Modal
+export default WorkModal
